fd68dc877cbed23736a5c79422c694f893203692
Christian <trnchristian94@gmail.com>
2019-10-23 12:57:45 +0200

    Updates

---
+++ precache-manifest.b1942b118d7dea9a7ce64cb572514c78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93efa5e7fa6dee33d35e72f298e5dfae",
+    "url": "/chrisreact/index.html"
+  },
+  {
+    "revision": "b649dba48b8fbc811288",
+    "url": "/chrisreact/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7960d33e985fb200cef8",
+    "url": "/chrisreact/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "b649dba48b8fbc811288",
+    "url": "/chrisreact/static/js/2.1e2cc5d2.chunk.js"
+  },
+  {
+    "revision": "7960d33e985fb200cef8",
+    "url": "/chrisreact/static/js/main.fa8ae035.chunk.js"
+  },
+  {
+    "revision": "6be596658c46f56b6683",
+    "url": "/chrisreact/static/js/runtime-main.2e6517a5.js"
+  }
+]);
